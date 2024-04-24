7bf22fb7de840c27c406536527419b0e77be46ed
hwazenhamdan <67022945+hwazenhamdan@users.noreply.github.com>
2024-04-24 21:41:33 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld{
         public static void main (String[] args){
 
                 System.out.println("Hello World ");
+                
                 System.out.println("Hello World again..");
+                
+                System.out.println("I hope this will trigger the job ");
                 System.out.println("I hope this will trigger the job ");
         }
 }
