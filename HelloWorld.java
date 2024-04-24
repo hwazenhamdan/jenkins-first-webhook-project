d462908d1244d9ecc51dc93d5a4d493bcfb973ad
hwazenhamdan <67022945+hwazenhamdan@users.noreply.github.com>
2024-04-24 21:39:04 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,8 @@ public class HelloWorld{
 
         public static void main (String[] args){
 
-                System.out.println("Hello World by Jenkins Job");
+                System.out.println("Hello World ");
+                System.out.println("Hello World again..");
+                System.out.println("I hope this will trigger the job ");
         }
 }
